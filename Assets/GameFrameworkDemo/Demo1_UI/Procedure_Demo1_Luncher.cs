3913492dad97f5579b6311ac53c2c0f0d3e0980b
changxinrui <641215320@qq.com>
2020-12-01 10:35:07 +0800

    [Test] WebGL资源热更通过。 Tip:需要在初始流程中调用Resource.InitResources()，webgl平台的AB资源包LoadType不可设置为LoadFromFile。

--- Assets/GameFrameworkDemo/Demo1_UI/Procedure_Demo1_Luncher.cs
+++ Assets/GameFrameworkDemo/Demo1_UI/Procedure_Demo1_Luncher.cs
@@ -1,8 +1,7 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GameFramework.Procedure;
 using UnityGameFramework.Runtime;
-using ProcedureOwner=GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
 
 namespace GameMain
 {
@@ -11,9 +10,25 @@ namespace GameMain
         protected override void OnEnter(ProcedureOwner procedureOwner)
         {
             base.OnEnter(procedureOwner);
+            GameEntry.Resource.InitResources(OnFinshed);
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+            if (isFinshed)
+            {
+                ChangeState<Procedure_Demo1_Menu>(procedureOwner);
+            }
+        }
+
+        public bool isFinshed = false;
+
+        private void OnFinshed()
+        {
             GameEntry.Scene.UnloadAllScene();
-            GameEntry.Scene.LoadScene("Assets/GameFrameworkDemo/Demo1_UI/Demo1_Menu.unity",this);
-            ChangeState<Procedure_Demo1_Menu>(procedureOwner);
+            GameEntry.Scene.LoadScene("Assets/GameFrameworkDemo/Demo1_UI/Demo1_Menu.unity", this);
+            isFinshed = true;
         }
     }
 }
