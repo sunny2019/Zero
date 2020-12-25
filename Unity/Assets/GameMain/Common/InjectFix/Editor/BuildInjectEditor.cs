4038588a5d65af7d071ef6d16102d2e5be5a5af0
changxinrui <641215320@qq.com>
2020-12-25 13:34:24 +0800

    [modify] 修改Injectfix文件路径

---
+++ Unity/Assets/GameMain/Common/InjectFix/Editor/BuildInjectEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameMain
+{
+    public class BuildInjectEditor
+    {
+        private const string PathchDir = "Assets/GameMain/Common/InjectFix/ByteFiles/";
+        private const string CustomOtherPatch = "Assembly-CSharp.patch.bytes";
+        private const string CustomAssetsAndPluginPatch = "Assembly-CSharp-filepass.patch.bytes";
+
+        [MenuItem("InjectFix/InjectAll", false, 1)]
+        static void InjectAll()
+        {
+
+            DeleteOldPatch();
+            Patch();
+            AssetDatabase.Refresh();
+        }
+
+        static void DeleteOldPatch()
+        {
+            if (File.Exists(PathchDir + CustomOtherPatch))
+            {
+                File.Delete(PathchDir + CustomOtherPatch);
+                File.Delete(PathchDir + CustomOtherPatch + ".meta");
+            }
+
+            if (File.Exists(PathchDir + CustomAssetsAndPluginPatch))
+            {
+                File.Delete(PathchDir + CustomAssetsAndPluginPatch);
+                File.Delete(PathchDir + CustomAssetsAndPluginPatch + ".meta");
+            }
+        }
+        static void Patch()
+        {
+            switch (Application.platform)
+            {
+                case RuntimePlatform.Android:
+                    IFix.Editor.IFixEditor.GenPlatformPatch(IFix.Editor.IFixEditor.Platform.android, "./" + PathchDir);
+                    IFix.Editor.IFixEditor.InjectAssemblys();
+                    break;
+                case RuntimePlatform.IPhonePlayer:
+                    IFix.Editor.IFixEditor.GenPlatformPatch(IFix.Editor.IFixEditor.Platform.ios, "./" + PathchDir);
+                    IFix.Editor.IFixEditor.InjectAssemblys();
+                    break;
+                default:
+                    IFix.Editor.IFixEditor.GenPlatformPatch(IFix.Editor.IFixEditor.Platform.standalone, "./" + PathchDir);
+                    IFix.Editor.IFixEditor.InjectAssemblys();
+                    break;
+            }
+        }
+    }
+}
